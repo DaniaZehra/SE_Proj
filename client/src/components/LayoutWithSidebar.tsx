7fe5dd7222f300d4ee845d13e4a7fff07245fa4d
DaniaZehra <daniazehra332@gmail.com>
2025-05-01 16:52:57 +0500

    layout with sidebar and without

---
+++ client/src/components/LayoutWithSidebar.tsx
@@ -0,0 +1,16 @@
+import { SidebarProvider, SidebarInset} from "@/components/ui/sidebar";
+import SidebarWithTrigger from "@/components/SidebarWithTrigger";
+import { AppSidebar } from "@/components/Sidebar";
+
+
+export default function LayoutWithSidebar({children}:Readonly<{children:React.ReactNode}>){
+    return(
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarWithTrigger/>
+            <SidebarInset>
+            {children}
+            </SidebarInset>
+          </SidebarProvider>
+    )
+}
