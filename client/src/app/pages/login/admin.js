3ef4dd67e48894009546957bf5a2fbdb18b6f2b6
vaniyarehan <vrehan33@gmail.com>
2025-05-02 20:26:57 +0500

    login pages

---
+++ client/src/app/pages/login/admin.js
@@ -0,0 +1,35 @@
+"use client"; 
+import LoginForm from "@/components/loginForm"; 
+import { useRouter } from "next/router";
+
+export default function AdminLoginPage() {
+  const router = useRouter();
+
+  const navigateToRegister = () => {
+    router.push("/register/admin");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-muted/50 p-6 rounded-md w-full max-w-sm shadow-md space-y-4 mx-auto mt-10">
+        <h2 className="text-xl font-semibold text-center">Login as Admin</h2>
+        
+        {}
+        <LoginForm role="admin" />
+
+        {}
+        <div className="text-center mt-4">
+          <p className="text-sm">
+            New to our app?{" "}
+            <span
+              onClick={navigateToRegister}
+              className="text-blue-500 cursor-pointer hover:underline"
+            >
+              Register now
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/app/pages/login/owner.js
@@ -0,0 +1,35 @@
+"use client"; 
+import LoginForm from "@/components/loginForm"; 
+import { useRouter } from "next/router";
+
+export default function OwnerLoginPage() {
+  const router = useRouter();
+
+  const navigateToRegister = () => {
+    router.push("/register/owner");
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-muted/50 p-6 rounded-md w-full max-w-sm shadow-md space-y-4 mx-auto mt-10">
+        <h2 className="text-xl font-semibold text-center">Login as Owner</h2>
+        
+        {}
+        <LoginForm role="owner" />
+
+        {}
+        <div className="text-center mt-4">
+          <p className="text-sm">
+            New to our app?{" "}
+            <span
+              onClick={navigateToRegister}
+              className="text-blue-500 cursor-pointer hover:underline"
+            >
+              Register now
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
