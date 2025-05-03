6e653a2b5d2aae56d9b2f670570089fbd982f88f
DaniaZehra <daniazehra332@gmail.com>
2025-05-03 11:20:52 +0500

    Property Models, bookingModels

---
+++ server/DBmodels/bookingModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+const {Schema, model, Types} = mongoose
+
+const propertyBookingsSchema = new Schema({
+    userId: Types.ObjectId,
+    propertyId: Types.ObjectId,
+    propertyName: String,
+    roomType: String,
+    guests: Number,
+    checkIn: Date,
+    checkOut: Date,
+    price: Number,
+    status: {type: String, enum: ['confirmed' | 'cancelled' | 'completed' | 'pending']},
+    createdAt: Date,
+    updatedAt: Date
+})
+  
+
+const propertyBooking = model('propertyBooking',propertyBookingsSchema)
+export {propertyBooking}
